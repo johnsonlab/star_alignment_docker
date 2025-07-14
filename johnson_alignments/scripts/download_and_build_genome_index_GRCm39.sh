50cf22d112a12eecda256ead2e4273b9c62d9e3d
Josh Johnson <jjohnson17@gmail.com>
2025-07-14 09:38:57 -0600

    Update download_and_build_genome_index_GRCh48.sh
    Changed Natalie's script (back) to human

--- johnson_alignments/scripts/download_and_build_genome_index_GRCm39.sh
+++ johnson_alignments/scripts/download_and_build_genome_index_GRCm39.sh
@@ -3,22 +3,28 @@
 script_dir=$(dirname "$(realpath "$0")")
 cd "${script_dir}/.."
 
-GENOMEDIR="./genome/M37"
+#GENOMEDIR="./genome/M37"
+GENOMEDIR="./genome/H48"
 
 mkdir -p ${GENOMEDIR}
 
 (
     cd ${GENOMEDIR}
     # download the "Genome sequence, primary assembly (GRCm39)" fasta file
-    [ -f GRCm39.primary_assembly.genome.fa.gz ] || \
-        wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M37/GRCm39.primary_assembly.genome.fa.gz
+    # Changed to human GRCh38
+    [ -f GRCh38.primary_assembly.genome.fa.gz ] || \
+    #[ -f GRCm39.primary_assembly.genome.fa.gz ] || \
+        wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_48/GRCh38.primary_assembly.genome.fa.gz
+  #      wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M37/GRCm39.primary_assembly.genome.fa.gz
     # filter it as described in the note below
     # download the annotations that correspond to it 
-    [ -f gencode.vM37.primary_assembly.annotation.gtf.gz ] || \
-        wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M37/gencode.vM37.primary_assembly.annotation.gtf.gz
+    [ -f gencode.v48.primary_assembly.annotation.gtf.gz ] || \
+    #[ -f gencode.vM37.primary_assembly.annotation.gtf.gz ] || \
+        wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_48/gencode.v48.primary_assembly.annotation.gtf.gz
+        #wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M37/gencode.vM37.primary_assembly.annotation.gtf.gz
 
-    gunzip -c GRCm39.primary_assembly.genome.fa.gz > GRCm39.primary_assembly.genome.fa
-    gunzip -c gencode.vM37.primary_assembly.annotation.gtf.gz > gencode.vM37.primary_assembly.annotation.gtf
+    gunzip -c GRCh38.primary_assembly.genome.fa.gz > GRCh38.primary_assembly.genome.fa
+    gunzip -c gencode.v48.primary_assembly.annotation.gtf.gz > gencode.v48.primary_assembly.annotation.gtf
 
 )
 
@@ -26,4 +32,4 @@ mkdir -p ${GENOMEDIR}/STAR
 STAR --runThreadN 4 --runMode genomeGenerate \
     --genomeDir ${GENOMEDIR}/STAR \
     --genomeFastaFiles ${GENOMEDIR}/GRCm39.primary_assembly.genome.fa \
-    --sjdbGTFfile ${GENOMEDIR}/gencode.vM37.primary_assembly.annotation.gtf.gz \
+    --sjdbGTFfile ${GENOMEDIR}/gencode.v48.primary_assembly.annotation.gtf.gz \
